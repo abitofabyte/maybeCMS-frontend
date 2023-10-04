291951c39d7814aecd7875aaf93972bfe795b691
ABitOfAByte <marcus.migotti@gmail.com>
2023-10-04 16:13:46 +0200

    used new TagSmall element

--- src/components/products/productCard.jsx
+++ src/components/products/productCard.jsx
@@ -1,8 +1,11 @@
+import TagSmall from "../tags/tagSmall.jsx";
+
 function ProductCard({product}) {
     return (<div>
         <h2>{product.name}</h2>
         <div>{product.description}</div>
-        ,
+        <div>{product.tag.map(tag => <TagSmall key={tag.id} tag={tag} />)}</div>
+
 
         <pre>{JSON.stringify(product, null, 2)}</pre>
     </div>)
