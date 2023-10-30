a0faf0e7caffcbcf04f9389327fec9d4e35c9d18
ABitOfAByte <marcus.migotti@gmail.com>
2023-10-30 15:55:21 +0100

    switch to bootstrap

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,53 +1,37 @@
-import style from "./navBar.module.css"
 import PropTypes from "prop-types";
-import {useLoaderData, useNavigate, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useLoaderData} from "react-router-dom";
 import LoginButton from "./LoginButton.jsx";
 import {useStoreContext} from "../../Store/StoreContext.jsx";
 import UserMenu from "./UserMenu.jsx";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 
 function NavBar({title}) {
     const categories = useLoaderData()
     const {user} = useStoreContext()
-    const {id} = useParams()
-    const navigate = useNavigate();
-    const [selected, setSelected] = useState(null)
-
-    function onSelectionClick(category) {
-        if (selected === category) {
-            setSelected(null)
-            navigate("/")
-
-        } else {
-            setSelected(category)
-            navigate("category/" + category.id)
-        }
-    }
-
-    function onHomeLinkClick() {
-        setSelected(null)
-        navigate("/")
-    }
-
-    useEffect(() => {
-        setSelected(categories.find(c => c.id === id))
-    }, [])
 
     return (
-        <nav className={style.nav}>
-            <div className={style.navBar}>
-                <div className="h1" onClick={onHomeLinkClick}>(╯°□°)╯︵ ┻━┻ {title}</div>
-                {user ? <UserMenu user={user}/> : <LoginButton/>}
-            </div>
-            <div className={style.buttons}>
-                {categories.map(category => <button
-                    className={selected === category ? style.selected : undefined}
-                    key={category.id}
-                    onClick={() => onSelectionClick(category)}
-                >{category.name}</button>)}
-            </div>
-        </nav>
+        <Navbar expand="md" sticky="top">
+            <Container>
+                <Navbar.Brand href="/">
+                    (╯°□°)╯︵ ┻━┻ {title}
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown id={"nav-dropdown"} title="Categories">
+                            {categories.map(category => <NavDropdown.Item
+                                key={category.id}
+                                href={"/category/" + category.id}
+                            >{category.name}</NavDropdown.Item>)}
+                        </NavDropdown>
+                        <Container className="align-self-end">
+                            {user ? <UserMenu user={user}/> : <LoginButton/>}
+                        </Container>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
